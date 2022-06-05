6e34a2559865d3c90c6c1d465472cf4909f4a8ff
Rushabh Modi <rushabh374@gmail.com>
2022-06-05 14:43:49 +0530

    server file modified

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const path = require("path");
 const express = require("express");
 const app = express();
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(__dirname + '/dist/personalkanban'));
 app.get('/*', function (req, res) {
-    res.sendFile(path.join(__dirname, 'dist', 'index.html'))
+    res.sendFile(path.join(__dirname, 'dist/personalkanban', 'index.html'))
 });
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
